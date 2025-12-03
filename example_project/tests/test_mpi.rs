7297d423d13ce194a808df885c7f3bac08af402f
philbucher <philipp.bucher@tum.de>
2025-12-04 00:01:28 +0100

    towards macro testing

--- example_project/tests/test_mpi.rs
+++ example_project/tests/test_mpi.rs
@@ -1,21 +1,16 @@
 use mpi_test_macros::mpi_test;
 use rstest::rstest;
 
-#[should_panic = "assertion failed: world.size() >= 45"]
 #[mpi_test(np = [2, 4])]
 fn simple_test() {
-    use mpi::traits::*;
-    let universe = mpi::initialize().unwrap();
-    let world = universe.world();
-    assert!(world.size() >= 45);
+    log_test_execution();
 }
 
-#[rstest]
-#[case(1)]
-#[case(2)]
-#[should_panic = "assertion failed: false"] // must be located after rstest stuff!
 #[mpi_test(np = [2, 4])]
-fn check_fail(#[case] _value: usize) {
+#[should_panic = "assertion failed: false"]
+fn simple_test_failure() {
+    log_test_execution();
+
     assert!(false);
 }
 
@@ -23,59 +18,54 @@ fn check_fail(#[case] _value: usize) {
 #[case(1)]
 #[case(2)]
 #[mpi_test(np = [2, 4])]
-fn my_param_test(#[case] value: usize) {
-    // Normal Rust test logic
+fn test_parametric(#[case] value: usize) {
     assert!(value == 1 || value == 2);
 
-    use mpi::traits::*;
+    log_test_execution();
+}
 
-    // MPI logic if needed
-    let universe = mpi::initialize().unwrap();
-    let world = universe.world();
-    assert!(world.rank() < world.size());
+#[rstest]
+#[case(1)]
+#[case(2)]
+#[should_panic = "assertion failed: false"] // must be located after rstest stuff!
+#[mpi_test(np = [2, 4])]
+fn test_parametric_fail(#[case] _value: usize) {
+    log_test_execution();
 
-    println!(
-        "XXXXX Process {} of {}: value = {}",
-        world.rank(),
-        world.size(),
-        value
-    );
+    assert!(false);
 }
 
 #[rstest]
 #[mpi_test(np = [2, 3])]
 fn test_with_values(#[values(10, 20, 30)] count: usize) {
-    // Verify the count value
     assert!(count == 10 || count == 20 || count == 30);
 
-    use mpi::traits::*;
-    let universe = mpi::initialize().unwrap();
-    let world = universe.world();
-
-    println!(
-        "Process {} of {}: testing with count = {}",
-        world.rank(),
-        world.size(),
-        count
-    );
+    log_test_execution();
 }
 
 #[rstest]
 #[mpi_test(np = [2,4])]
 fn test_cartesian_matrix(#[values(1, 2)] x: usize, #[values("a", "b", "c")] y: &str) {
-    // Verify the values
     assert!(x == 1 || x == 2);
     assert!(y == "a" || y == "b" || y == "c");
 
+    log_test_execution();
+}
+
+mod my_module {
+    use super::*;
+
+    #[mpi_test(np = [2, 4])]
+    fn module_test() {
+        log_test_execution();
+    }
+}
+
+/// Logs the execution of the test from all MPI ranks.
+/// used to check that all ranks reach the test body
+fn log_test_execution() {
     use mpi::traits::*;
     let universe = mpi::initialize().unwrap();
     let world = universe.world();
-
-    println!(
-        "Process {} of {}: testing x={}, y={}",
-        world.rank(),
-        world.size(),
-        x,
-        y
-    );
+    eprintln!("RANK {} reached test body", world.rank());
 }
